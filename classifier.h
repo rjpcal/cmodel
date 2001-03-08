59fe175eeda4fc225794e98f24e9efa8b78e414f
Robert J. Peters <rjpcal@gmail.com>
2001-03-08 18:55:15 +0000

    Moved all the mex stuff to classifier_mex.cc, leaving just C++ here.

--- classifier.h
+++ classifier.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Mar  8 09:48:36 2001
-// written: Thu Mar  8 09:58:28 2001
+// written: Thu Mar  8 10:51:19 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,18 +17,69 @@
 #define MLF_V2 1
 #endif
 
-#include "libmatlb.h"
+class Rat;
 
-extern void InitializeModule_classifier(void);
-extern void TerminateModule_classifier(void);
-extern _mexLocalFunctionTable _local_function_table_classifier;
+class Classifier {
+public:
+  static void forwardProbit(double* diffEvidence,
+									 int num_points,
+									 double thresh_,
+									 double sigmaNoise_,
+									 double* ppOut);
 
-extern mxArray* mlfClassifier(mxArray* modelParams,
-										mxArray* objParams,
-										mxArray* observedIncidence,
-										mxArray* numExemplars);
+  static double loglikelihood(const double* predictedProbability1,
+										int numPoints,
+										const double* observedIncidence1,
+										const double* observedIncidence2);
+};
 
-extern void mlxClassifier(int nlhs, mxArray* plhs[], int nrhs, mxArray* prhs[]);
+class ModelCssm : public Classifier {
+private:
+  const Rat& objParams;
+  const Rat& observedIncidence;
+  const int numStoredExemplars;
+  const int num1;
+  const int num2;
+  const int numTrainingExemplars;
+  const int numAllExemplars;
+
+  const int dimObjParams;
+
+  double* const mu;
+
+  typedef const double* constDblPtr;
+
+  constDblPtr* const cat1;
+  constDblPtr* const cat2;
+
+  // Count the category training exemplars
+  int countCategory(const Rat& params, int category);
+
+  void resetMu()
+  {
+    for (int i = 0; i < numAllExemplars; ++i)
+      mu[i] = 0.0;
+  }
+
+  void computeSimilarity(const double* attWeights,
+								 const double* storedExemplar1,
+								 const double* storedExemplar2,
+								 double minkPower,
+								 double minkPowerInv);
+
+  void computeSimilarity2(const double* attWeights,
+								  const double* storedExemplar1,
+								  const double* storedExemplar2);
+
+public:
+  ModelCssm(const Rat& objParams_,
+				const Rat& observedIncidence_,
+				int numStoredExemplars_);
+
+  ~ModelCssm();
+
+  double loglikelihoodFor(Rat& modelParams);
+};
 
 static const char vcid_classifier_h[] = "$Header$";
 #endif // !CLASSIFIER_H_DEFINED
