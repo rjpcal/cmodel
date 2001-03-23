2cd65a4f2df1efb949608dcc7a6117edbed581c2
Robert J. Peters <rjpcal@gmail.com>
2001-03-23 20:13:25 +0000

    Removed the unused "path_list".

--- classifier_mex.cc
+++ classifier_mex.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Mar  8 09:49:21 2001
-// written: Wed Mar 21 14:07:35 2001
+// written: Fri Mar 23 12:12:00 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -328,10 +328,9 @@ DOTRACE("mlxClassifier");
 ///////////////////////////////////////////////////////////////////////
 //
 // The function "mexLibrary" is a Compiler-generated mex wrapper,
-// suitable for building a MEX-function. It initializes any persistent
-// variables as well as a function table for use by the feval
-// function. It then calls the function "mlxClassifier". Finally, it
-// clears the feval table and exits.
+// which (apparently, since I can't find any docs for this API)
+// returns information about the functions provided in the containing
+// MEX file.
 //
 ///////////////////////////////////////////////////////////////////////
 
@@ -346,14 +345,12 @@ mex_information mexLibrary() {
 #endif
   };
 
-  static const char * path_list_[1] = { "/matlab_r.12/toolbox/matlab/specfun" };
-
   static _mexInitTermTableEntry init_term_table[1] = {
 	 { InitializeModule_classifier, TerminateModule_classifier },
   };
 
   static _mex_information _mex_info
-	 = { 1, 1, function_table, 0, NULL, 1, path_list_, 1, init_term_table };
+	 = { 1, 1, function_table, 0, NULL, 0, NULL, 1, init_term_table };
 
   return &_mex_info;
 }
