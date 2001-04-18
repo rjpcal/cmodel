c8bf88bda37fa37faf185ad4decce28418b3c4d5
Robert J. Peters <rjpcal@gmail.com>
2001-04-18 21:59:19 +0000

    Initial revision

---
+++ simplexoptimizer.cc
@@ -0,0 +1,225 @@
+///////////////////////////////////////////////////////////////////////
+//
+// simplexoptimizer.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Apr 18 14:52:57 2001
+// written: Wed Apr 18 14:59:00 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef SIMPLEXOPTIMIZER_CC_DEFINED
+#define SIMPLEXOPTIMIZER_CC_DEFINED
+
+#include "simplexoptimizer.h"
+
+#include "strings.h"
+
+#include <iostream.h>
+#include <iomanip.h>
+
+#include "trace.h"
+
+namespace
+{
+  SimplexOptimizer::PrintType extractPrinttype(const fixed_string& printtype)
+  {
+	 if      (printtype == "notify")   return SimplexOptimizer::NOTIFY;
+	 else if (printtype == "none")     return SimplexOptimizer::NONE;
+	 else if (printtype == "off")      return SimplexOptimizer::NONE;
+	 else if (printtype == "iter")     return SimplexOptimizer::ITER;
+	 else if (printtype == "final")    return SimplexOptimizer::FINAL;
+	 else if (printtype == "simplex")  return SimplexOptimizer::SIMPLEX;
+
+	 return SimplexOptimizer::NOTIFY;
+  }
+}
+
+SimplexOptimizer::SimplexOptimizer(MultivarFunction& objective,
+											  const Mtx& x_in,
+											  const fixed_string& printtype,
+											  const double tolx,
+											  const double tolf,
+											  const int nparams,
+											  const int maxfun,
+											  const int maxiter) :
+  itsObjective(objective),
+
+  itsInitialParams(x_in.asColumn()), // Set up simplex near the initial guess
+  itsPrnt(extractPrinttype(printtype)),
+  itsTolx(tolx),
+  itsTolf(tolf),
+  itsNparams(nparams),
+  itsMaxFevals(maxfun),
+  itsMaxIters(maxiter),
+
+  itsSimplex(nparams, nparams+1),
+  itsFvals(1, nparams+1),
+
+  itsIterCount(1)
+{
+DOTRACE("SimplexOptimizer::SimplexOptimizer");
+  // Place input guess in the simplex! (credit L.Pfeffer at Stanford)
+  putInSimplex(itsInitialParams, 0);
+
+  buildInitialSimplex();
+
+  minimalSort();
+}
+
+void SimplexOptimizer::printIter()
+{
+  if (itsPrnt == ITER)
+	 {
+		cerr << setw(6) << itsIterCount
+			  << setw(13) << funcCount()
+			  << setw(17) << setprecision(6) << itsFvals.at(0,0)
+			  << "         " << iterTypeString(itsCurIter) << '\n';
+	 }
+}
+
+void SimplexOptimizer::printSimplex()
+{
+  if (itsPrnt == SIMPLEX)
+	 {
+		cerr << '\n' << iterTypeString(itsCurIter) << '\n';
+		itsSimplex.print("simplex");
+		itsFvals.print("fvals");
+		cerr << "funcEvals: " << funcCount() << '\n';
+	 }
+}
+
+void SimplexOptimizer::minimalSort()
+{
+DOTRACE("SimplexOptimizer::minimalSort");
+
+  Mtx index = itsFvals.row(0).getSortOrder();
+
+  const int smallest = int(index.at(0));
+  const int largest = int(index.at(itsNparams));
+  const int largest2 = int(index.at(itsNparams-1));
+
+  // These swaps are smart enough to check if the column numbers
+  // are the same before doing the swap
+
+  itsFvals.swapColumns(0, smallest);
+  itsFvals.swapColumns(largest, itsNparams);
+  itsFvals.swapColumns(largest2, itsNparams-1);
+
+  itsSimplex.swapColumns(0, smallest);
+  itsSimplex.swapColumns(largest, itsNparams);
+  itsSimplex.swapColumns(largest2, itsNparams-1);
+}
+
+int SimplexOptimizer::optimize()
+{
+  printHeader();
+  itsCurIter = INITIAL;
+  printIter();
+  printSimplex();
+
+  // Iterate until the diameter of the simplex is less than itsTolx AND
+  // the function values differ from the min by less than itsTolf, or
+  // the max function evaluations are exceeded. (Cannot use OR
+  // instead of AND.)
+
+  while (!withinTolf() || !withinTolx())
+	 {
+		if (tooManyIters()) return 0;
+		if (tooManyFevals()) return 0;
+
+		doOneIter();
+	 } // end main algorithm loop
+
+  cerr << "\nOptimization terminated successfully:\n"
+		 << " the current x satisfies the termination criteria using "
+		 << "OPTIONS.TolX of " << itsTolx << '\n'
+		 << " and F(X) satisfies the convergence criteria using "
+		 << "OPTIONS.TolFun of " << itsTolf << '\n';
+
+  return 1;
+}
+
+
+void SimplexOptimizer::doOneIter()
+{
+DOTRACE("SimplexOptimizer::doOneIter");
+
+  // compute average of the itsNparams (NOT itsNparams+1) best points
+  Mtx xbar(itsSimplex.columns(0,itsNparams).meanColumn());
+
+  // Direction along which to reflect/expand/contract
+  Mtx direction(xbar - itsSimplex.columns(itsNparams,1));
+
+  // Calculate the reflection point
+  FuncPoint rflPt = evaluate(xbar + direction);
+
+  if (rflPt.betterThan(simplexAt(0)))
+ 	 {
+ 		DOTRACE("rflPt.betterThan(simplexAt(0))");
+
+ 		// Calculate the expansion point
+ 		FuncPoint expPt = evaluate(xbar + direction*2.0);
+
+ 		if (expPt.betterThan(rflPt))
+ 		  { putInSimplex(expPt, itsNparams); itsCurIter = EXPAND; }
+ 		else
+ 		  { putInSimplex(rflPt, itsNparams); itsCurIter = REFLECT; }
+ 	 }
+  else
+	 {
+		DOTRACE("!addReflection");
+
+		if (rflPt.betterThan(simplexAt(itsNparams-1)))
+		  { putInSimplex(rflPt, itsNparams); itsCurIter = REFLECT; }
+		else
+		  {
+			 // Perform contraction
+
+			 if (rflPt.betterThan(simplexAt(itsNparams)))
+				{
+				  // Perform an outside contraction
+				  FuncPoint ictPt = evaluate(xbar + direction*0.5);
+
+				  if (ictPt.betterThan(rflPt))
+					 { putInSimplex(ictPt, itsNparams); itsCurIter = CONTRACT_OUTSIDE; }
+				  else
+					 { itsCurIter = SHRINK; }
+				}
+			 else
+				{
+				  // Perform an inside contraction
+				  FuncPoint octPt = evaluate(xbar - direction*0.5);
+
+				  if (octPt.betterThan(simplexAt(itsNparams)))
+					 { putInSimplex(octPt, itsNparams); itsCurIter = CONTRACT_INSIDE; }
+				  else
+					 { itsCurIter = SHRINK; }
+				}
+
+			 if (SHRINK == itsCurIter)
+				{
+				  for (int j = 1; j < itsNparams+1; ++j)
+					 {
+						putInSimplex(itsSimplex.columns(0,1) +
+										 (itsSimplex.columns(j,1) -
+										  itsSimplex.columns(0,1)) * 0.5,
+										 j);
+					 }
+				}
+		  }
+	 }
+
+  minimalSort();
+
+  ++itsIterCount;
+
+  printIter();
+  printSimplex();
+}
+
+
+static const char vcid_simplexoptimizer_cc[] = "$Header$";
+#endif // !SIMPLEXOPTIMIZER_CC_DEFINED
---
+++ simplexoptimizer.h
@@ -0,0 +1,247 @@
+///////////////////////////////////////////////////////////////////////
+//
+// simplexoptimizer.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Apr 18 14:52:30 2001
+// written: Wed Apr 18 14:58:27 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef SIMPLEXOPTIMIZER_H_DEFINED
+#define SIMPLEXOPTIMIZER_H_DEFINED
+
+#include "mtx.h"
+#include "multivarfunction.h"
+
+class fixed_string;
+
+class SimplexOptimizer {
+private:
+  MultivarFunction& itsObjective;
+
+  const Mtx itsInitialParams;
+  const int itsPrnt;
+  const double itsTolx;
+  const double itsTolf;
+  const int itsNparams;
+  const int itsMaxFevals;
+  const int itsMaxIters;
+
+  Mtx itsSimplex;
+  Mtx itsFvals;
+
+  int itsIterCount;
+
+  enum IterType { INITIAL, EXPAND, REFLECT, CONTRACT_OUTSIDE, CONTRACT_INSIDE, SHRINK };
+
+  IterType itsCurIter;
+
+  void buildInitialSimplex()
+  {
+	 // Following improvement suggested by L.Pfeffer at Stanford
+	 // 5 percent deltas for non-zero terms
+	 const double usual_delta = 0.05;
+
+	 // Even smaller delta for zero elements of x
+	 const double zero_term_delta = 0.00025;
+
+	 for (int j = 0; j < itsNparams; ++j)
+		{
+		  itsSimplex.column(j+1) = itsInitialParams;
+
+		  if (itsSimplex.at(j,j+1) != 0.0)
+			 itsSimplex.at(j,j+1) *= (1.0 + usual_delta);
+		  else
+			 itsSimplex.at(j,j+1) = zero_term_delta;
+
+		  itsFvals.at(0,j+1) = itsObjective.evaluate(itsSimplex.column(j+1));
+		}
+  }
+
+  struct FuncPoint
+  {
+	 FuncPoint(const Mtx& x_, double f_) : x(x_), f(f_) {}
+
+	 const Mtx x;
+	 const double f;
+
+	 bool betterThan (const FuncPoint& other)
+	   { return (f < other.f); }
+  };
+
+  FuncPoint evaluate(const Mtx& x)
+  {
+	 return FuncPoint(x, itsObjective.evaluate(x));
+  }
+
+  void putInSimplex(const FuncPoint& p, int pointNumber)
+  {
+	 itsSimplex.column(pointNumber) = p.x.column(0);
+	 itsFvals.at(0, pointNumber) = p.f;
+  }
+
+  void putInSimplex(const Mtx& params, int simplexPoint)
+  {
+	 putInSimplex(evaluate(params), simplexPoint);
+  }
+
+  FuncPoint simplexAt(int simplexPoint)
+  {
+	 return FuncPoint(itsSimplex.column(simplexPoint),
+							itsFvals.at(0, simplexPoint));
+  }
+
+#if 0
+  void fullSort()
+  {
+	 // sort so itsSimplex.column(0) has the lowest function value 
+	 Mtx index = itsFvals.row(0).getSortOrder();
+
+	 itsFvals.row(0).reorder(index);
+  	 itsSimplex.reorderColumns(index);
+  }
+#endif
+
+  void minimalSort();
+
+  bool withinTolf()
+  {
+	 MtxConstIter fvals = itsFvals.rowIter(0);
+	 const double f0 = *fvals;
+	 ++fvals;
+
+	 for (; fvals.hasMore(); ++fvals)
+		{
+		  if (fabs(*fvals - f0) > itsTolf)
+			 return false;
+		}
+
+	 return true;
+  }
+
+  bool withinTolx()
+  {
+	 const MtxConstIter col0_ = itsSimplex.columnIter(0);
+
+	 for (int col = 1; col < itsSimplex.ncols(); ++col)
+		{
+		  MtxConstIter col0(col0_);
+		  MtxConstIter coln = itsSimplex.columnIter(col);
+
+		  for (; col0.hasMore(); ++col0, ++coln)
+			 if ( fabs(*col0 - *coln) > itsTolx ) return false;
+		}
+
+	 return true;
+  }
+
+  bool tooManyFevals()
+  {
+	 if (funcCount() < itsMaxFevals)
+		return false;
+
+	 if (itsPrnt != NONE) {
+		cerr << "\nExiting: Maximum number of function evaluations "
+			  << "has been exceeded\n"
+			  << "         - increase MaxFunEvals option.\n"
+			  << "         Current function value: "
+			  << bestFval()
+			  << "\n\n";
+	 }
+
+	 return true;
+  }
+
+  bool tooManyIters()
+  {
+	 if (itsIterCount < itsMaxIters)
+		return false;
+
+	 if (itsPrnt != NONE) {
+		cerr << "\nExiting: Maximum number of iterations "
+			  << "has been exceeded\n"
+			  << "         - increase MaxIter option.\n"
+			  << "         Current function value: "
+			  << bestFval()
+			  << "\n\n";
+	 }	 
+
+	 return true;
+  }
+
+  void doOneIter();
+
+  void printHeader()
+  {
+	 if (itsPrnt == ITER)
+		cerr << "\n Iteration   Func-count     min f(x)         Procedure\n";
+  }
+
+  static const char* iterTypeString(IterType how)
+  {
+	 switch (how)
+		{
+		case INITIAL:
+		  return "initial";
+		  break;
+		case EXPAND:
+		  return "expand";
+		  break;
+		case REFLECT:
+		  return "reflect";
+		  break;
+		case CONTRACT_OUTSIDE:
+		  return "contract outside";
+		  break;
+		case CONTRACT_INSIDE:
+		  return "contract inside";
+		  break;
+		case SHRINK:
+		  return "shrink";
+		  break;
+		default:
+		  return "unknown";
+		  break;
+		}
+  }
+
+  void printIter();
+
+  void printSimplex();
+
+public:
+  enum PrintType { NONE, NOTIFY, FINAL, ITER, SIMPLEX };
+
+  SimplexOptimizer(MultivarFunction& objective,
+						 const Mtx& x_in,
+						 const fixed_string& printtype,
+						 const double tolx,
+						 const double tolf,
+						 const int nparams,
+						 const int maxfun,
+						 const int maxiter);
+  virtual ~SimplexOptimizer() {}
+
+  int optimize();
+
+  virtual Mtx bestParams()
+  { return itsSimplex.column(0); }
+
+  virtual double bestFval()
+  { return itsFvals.at(0,0); }
+
+  virtual int iterCount()
+  { return itsIterCount; }
+
+  virtual int funcCount()
+  { return itsObjective.evalCount(); }
+
+  virtual const char* algorithm()
+  { return "Nelder-Mead simplex direct search"; }
+};
+
+static const char vcid_simplexoptimizer_h[] = "$Header$";
+#endif // !SIMPLEXOPTIMIZER_H_DEFINED
