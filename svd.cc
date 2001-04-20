f15f31400d316cfb2d591de1fe7c222e19c11773
Robert J. Peters <rjpcal@gmail.com>
2001-04-20 00:32:09 +0000

    Moved stuff into nrutil.h.

--- svd.cc
+++ svd.cc
@@ -2,65 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 
-#define NR_END 1
-
-#define SIGN(a,b) ((b) >= 0.0 ? fabs(a) : -fabs(a))
-
-inline double FMAX(double a, double b)
-{ return (a > b) ? a : b; }
-
-inline int IMIN(int a, int b)
-{ return (a < b) ? a : b; }
-
-inline double SQR(double a) { return a*a; }
-
-double pythag(double a, double b)
-{
-  double absa,absb;
-  absa = fabs(a);
-  absb = fabs(b);
-  if (absa > absb) return absa*sqrt(1.0+SQR(absb/absa));
-  else return (absb == 0.0 ? 0.0 : absb*sqrt(1.0+SQR(absa/absb)));
-}
-
-double* vector(long nl, long nh)
-{
-  double* v;
-  v = (double*) malloc((size_t) ((nh-nl+1+NR_END)*sizeof(double)));
-  return v-nl+NR_END;
-}
-
-void free_vector(double* v, long nl, long nh)
-{
-  free(v+nl-NR_END);
-}
-
-double** matrix(long nrl, long nrh, long ncl, long nch)
-{
-  long i, nrow=nrh-nrl+1, ncol=nch-ncl+1;
-  double **m;
-
-  m = (double**) malloc((size_t)((nrow+NR_END)*sizeof(double*)));
-
-  m += NR_END;
-  m -= nrl;
-
-  m[nrl]=(double*) malloc((size_t)((nrow*ncol+NR_END)*sizeof(double)));
-
-  m[nrl] += NR_END;
-  m[nrl] -= ncl;
-
-  for (i=nrl+1;i<=nrh;++i)
-	 m[i]=m[i-1]+ncol;
-
-  return m;
-}
-
-void free_matrix(double **m, long nrl, long nrh, long ncl, long nch)
-{
-  free(m[nrl]+ncl-NR_END);
-  free(m+nrl-NR_END);
-}
+#include "nrutil.h"
 
 void svdcmp(
 				double **a,
