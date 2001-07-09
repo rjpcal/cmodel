1b695fc661fb56622e692e289c1ead10229af6a2
Robert J. Peters <rjpcal@gmail.com>
2001-07-09 21:06:05 +0000

    Initial revision

---
+++ minkbinder.cc
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////////////////////////////
+//
+// minkbinder.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Mon Jul  9 13:59:45 2001
+// written: Mon Jul  9 13:59:58 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MINKBINDER_CC_DEFINED
+#define MINKBINDER_CC_DEFINED
+
+#include "minkbinder.h"
+
+//#define USE_ASM
+
+double MinkowskiBinder::minkDist2(MtxConstIter x1) const
+{
+#ifndef USE_ASM
+
+  double wt_sum = 0.0;
+  MtxConstIter wt = itsAttWeights;
+  MtxConstIter x2 = itsX2;
+
+  for (; wt.hasMore(); ++wt, ++x1, ++x2)
+    {
+      wt_sum += (*wt) * ((*x1) - (*x2)) * ((*x1) - (*x2));
+    }
+  return sqrt(wt_sum);
+
+#else
+
+// layout of *this
+//      12 bytes -- wts
+// +0        4 bytes -- data pointer
+// +4        4 bytes -- stride
+// +8        4 bytes -- stop pointer
+//      12 bytes -- x2
+// +12       4 bytes -- data pointer
+// +16       4 bytes -- stride
+// +20       4 bytes -- stop pointer
+// +24  8 bytes  -- itsR
+// +32  8 bytes  -- itsRinv
+
+asm(
+    // "pushl %ebp" assumed
+    // "movl %esp,%ebp" assumed
+
+    "subl $4,%esp;"             // {1c} 4b on stack
+    "movl 8(%ebp),%edx;"        // {1c} copy the "this" pointer to edx
+
+    "fldz;"                     // {2c} initialize the result to 0.0
+
+    "pushl %edi;"               // {1c}
+    "pushl %esi;"               // {1c}
+    "pushl %ebx;"               // {1c}
+
+#define WTS_REG "%eax"
+    "movl (%edx),"WTS_REG";"    // {1c} copy wts.data to WTS_REG
+
+    "movl 8(%edx),%ebx;"        // {1c} copy wts.stop to ebx
+
+#define X2_REG "%ecx"
+    "movl 12(%edx),"X2_REG";"   // {1c} copy x2.data to X2_REG
+
+    "cmpl %ebx,"WTS_REG";"      // {1c}
+    "jae cleanup;"              // {3/1c} jump if (wts.data >= wts.stop)
+
+    "movl 4(%edx),%edi;"        // {1c} copy wts.stride to edi
+    "sall $3,%edi;"             // {2c} wts.stride *= 8 (which is sizeof(double))
+    "movl %edi,-4(%ebp);"       // {1c} copy wts.stride to stack-4
+
+    "movl 16(%ebp),%edi;"       // {1c} copy x1.stride to edi
+    "sall $3,%edi;"             // {2c} x1.stride *= 8
+
+    "movl 16(%edx),%esi;"       // {1c} copy x2.stride to esi
+
+    "sall $3,%esi;"             // {2c} x2.stride *= 8
+    ".p2align 4,,7;"
+
+#define X1_REG "%edx"
+
+    "movl 12(%ebp),"X1_REG";"   // {1c} copy x1.data to edx
+
+"sum_loop:;"
+    "fldl ("X1_REG");"          // {2c} top = *x1.data
+    "fsubl ("X2_REG");"         // {3/1c} top -= *x2.data
+    "fldl ("WTS_REG");"         // {2c} top = *wts.data
+    "fmul %st(1),%st;"          // {3/1c} top *= (*x1.data - *x2.data)
+
+    "addl -4(%ebp),"WTS_REG";"  // {2c} wts.data += wts.stride
+    "addl %edi,"X1_REG";"       // {1c} x1.data += x1.stride
+    "addl %esi,"X2_REG";"       // {1c} x2.data += x2.stride
+
+    "fmulp %st,%st(1);"         // {3/1c} top *= (*x1.data - *x2.data) and pop st(0)
+    "faddp %st,%st(1);"         // {3/1c} add term to wt_sum and pop st(0)
+
+    "cmpl %ebx,"WTS_REG";"      // {1c}
+    "jb sum_loop;"              // {3/1c} loop if (wts.data < wts.stop)
+
+    "fsqrt;"                    // {70c} st(0) = sqrt(wt_sum)
+
+"cleanup:;"
+    "leal -16(%ebp),%esp;"      // {1c}
+    "popl %ebx;"                // {4c}
+    "popl %esi;"                // {4c}
+    "popl %edi;"                // {4c}
+    // "movl %ebp,%esp;" assumed
+    // "popl %ebp;" assumed
+    // "ret;" assumed
+    ); // end asm
+#endif
+}
+
+static const char vcid_minkbinder_cc[] = "$Header$";
+#endif // !MINKBINDER_CC_DEFINED
---
+++ minkbinder.h
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////
+//
+// minkbinder.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Mon Jul  9 13:59:12 2001
+// written: Mon Jul  9 13:59:24 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MINKBINDER_H_DEFINED
+#define MINKBINDER_H_DEFINED
+
+#include "mtx.h"
+
+//
+// This is just a functor that binds arguments to minkDist, so that
+// we don't have to keep passing extra arguments to functions all the
+// time in the time-critical inner loop.
+//
+
+class MinkowskiBinder {
+public:
+  MinkowskiBinder(MtxConstIter attWeights, MtxConstIter x2,
+                   double r = 2.0, double r_inv = 0.5) :
+    itsAttWeights(attWeights),
+    itsX2(x2),
+    itsR(r),
+    itsRinv(r_inv)
+  {}
+
+  double minkDist(MtxConstIter x1) const
+  {
+    double wt_sum = 0.0;
+    MtxConstIter wt = itsAttWeights;
+    MtxConstIter x2 = itsX2;
+
+    for (; wt.hasMore(); ++wt, ++x1, ++x2)
+      {
+        wt_sum += (*wt) * pow( abs( *x1 - *x2), itsR);
+      }
+    return pow(wt_sum, itsRinv);
+  }
+
+  // Specialized Minkowski distance for r==2
+  double minkDist2(MtxConstIter x1) const;
+
+private:
+  const MtxConstIter itsAttWeights;
+  const MtxConstIter itsX2;
+  const double itsR;
+  const double itsRinv;
+};
+
+static const char vcid_minkbinder_h[] = "$Header$";
+#endif // !MINKBINDER_H_DEFINED
