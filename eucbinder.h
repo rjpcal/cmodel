3a16d7dabe9b23ebb3042fd1861f0d4c884fd810
Robert J. Peters <rjpcal@gmail.com>
2002-02-08 00:27:02 +0000

    Initial revision

---
+++ eucbinder.h
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////
+//
+// eucbinder.h
+//
+// Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Feb  7 16:18:49 2002
+// written: Thu Feb  7 16:20:24 2002
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef EUCBINDER_H_DEFINED
+#define EUCBINDER_H_DEFINED
+
+#include "mtx.h"
+
+//
+// This is just a functor that binds arguments to eucDist, so that we don't
+// have to keep passing extra arguments to functions all the time in the
+// time-critical inner loop.
+//
+// In addition, this class is templatized on the number of elements in each
+// vector, so that loops can be better optimized at compile-time. Therefore
+// note that no range-checking will be done at run-time.
+//
+
+template <unsigned int Dim>
+class EuclideanBinder
+{
+public:
+  EuclideanBinder(MtxConstIter attWeights, MtxConstIter x2) :
+    itsAttWeightsStop(&itsAttWeights[0] + Dim)
+  {
+    double* wtptr = &itsAttWeights[0];
+    double* x2ptr = &itsX2[0];
+
+    for (; wtptr < itsAttWeightsStop; ++wtptr, ++x2ptr, ++attWeights, ++x2)
+      {
+        *wtptr = *attWeights;
+        *x2ptr = *x2;
+      }
+  }
+
+  double eucDist(MtxConstIter x1) const
+  {
+    double wt_sum = 0.0;
+
+    const double* wt = &itsAttWeights[0];
+    const double* x2 = &itsX2[0];
+
+    for (; wt < itsAttWeightsStop; ++wt, ++x2, ++x1)
+      {
+        wt_sum += (*wt) * square((*x1) - (*x2));
+      }
+    return sqrt(wt_sum);
+  }
+
+private:
+  static double square(double x) { return x * x; }
+
+  double itsAttWeights[Dim];
+  double* const itsAttWeightsStop;
+  double itsX2[Dim];
+};
+
+static const char vcid_eucbinder_h[] = "$Header$";
+#endif // !EUCBINDER_H_DEFINED
