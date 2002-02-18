6722ab3f7ff02a5f30553195923d0d6e847b1f01
Robert J. Peters <rjpcal@gmail.com>
2002-02-18 22:33:55 +0000

    Moved MatlabFunction into a separate module, and refactored things a bit.

--- doSimplex.cc
+++ doSimplex.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 2001-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Sun Apr  1 19:52:50 2001
-// written: Thu Feb 14 14:13:39 2002
+// written: Mon Feb 18 14:33:36 2002
 // $Id$
 //
 //
@@ -25,61 +25,21 @@
 
 #include "doSimplex.h"
 
-#include "util/error.h"
 #include "mexbuf.h"
 #include "mtx.h"
 #include "mxwrapper.h"
-#include "multivarfunction.h"
+#include "matlabfunction.h"
 #include "simplexoptimizer.h"
+
+#include "util/error.h"
 #include "util/strings.h"
 
 #include <iostream.h>
 #include <iomanip.h>
-#include "libmatlbm.h"
+#include <libmatlbm.h>
 
 #define LOCAL_PROF
 #include "util/trace.h"
-
-class MatlabFunction : public MultivarFunction
-{
-  mxArray* itsFunfcn;
-  mxArray* itsVarargin_ref;
-
-  static mxArray* getref(mxArray* varargin)
-    {
-      return mlfIndexRef(varargin,
-                         "{?}",
-                         mlfCreateColonIndex());
-    }
-
-  double evaluate_mx(mxArray* x_mx)
-  {
-    DOTRACE("evaluate_mx");
-
-    mxArray* mx =  mlfFeval(mclValueVarargout(),
-                            itsFunfcn,
-                            x_mx,
-                            getref(itsVarargin_ref),
-                            NULL);
-    double result = mxGetScalar(mx);
-    mxDestroyArray(mx);
-    return result;
-  }
-
-  virtual double doEvaluate(const Mtx& x)
-  {
-    return evaluate_mx(x.makeMxArray());
-  }
-
-public:
-  MatlabFunction(mxArray* funfcn_mx, mxArray* varargin_mx) :
-    MultivarFunction(),
-    itsFunfcn(funfcn_mx),
-    itsVarargin_ref(varargin_mx)
-  {}
-
-  virtual ~MatlabFunction() {}
-};
 
 int extractMaxIters(const mxArray* arr, int numModelParams)
 {
@@ -136,101 +96,6 @@ void TerminateModule_doSimplex(void)
   delete mexBuf;
 }
 
-static mxArray * MdoSimplex(mxArray * * fval,
-                            mxArray * * exitflag,
-                            mxArray * * output,
-                            int nargout_,
-                            mxArray * funfcn,
-                            mxArray * x_in,
-                            mxArray * printtype,
-                            mxArray * tolx,
-                            mxArray * tolf,
-                            mxArray * maxfun,
-                            mxArray * maxiter,
-                            mxArray * debugFlags,
-                            mxArray * varargin);
-
-/*
- * The function "mlxDoSimplex" contains the feval interface for the "doSimplex"
- * M-function from file
- * "/cit/rjpeters/science/psyphy/classmodels/matlab/doSimplex.m" (lines 1-237).
- * The feval function calls the implementation version of doSimplex through
- * this function. This function processes any input arguments and passes them
- * to the implementation version of the function, appearing above.
- */
-void mlxDoSimplex(int nlhs, mxArray * plhs[], int nrhs, mxArray * prhs[])
-{
-  mxArray * mprhs[9];
-  mxArray * mplhs[4];
-  int i;
-  if (nlhs > 4)
-    {
-      mexErrMsgTxt("Run-time Error: File: doSimplex Line: 1 Column: 1 "
-                   "The function \"doSimplex\" was called with more "
-                   "than the declared number of outputs (4).");
-    }
-  for (i = 0; i < 4; ++i)
-    {
-      mplhs[i] = mclGetUninitializedArray();
-    }
-  for (i = 0; i < 8 && i < nrhs; ++i)
-    {
-      mprhs[i] = prhs[i];
-    }
-  for (; i < 8; ++i)
-    {
-      mprhs[i] = NULL;
-    }
-  mlfEnterNewContext(0,
-                     8,
-                     mprhs[0],
-                     mprhs[1],
-                     mprhs[2],
-                     mprhs[3],
-                     mprhs[4],
-                     mprhs[5],
-                     mprhs[6],
-                     mprhs[7]);
-  mprhs[8] = NULL;
-  mlfAssign(&mprhs[8], mclCreateVararginCell(nrhs - 8, prhs + 8));
-
-  mplhs[0] = MdoSimplex(&mplhs[1],
-                        &mplhs[2],
-                        &mplhs[3],
-                        nlhs,
-                        mprhs[0],
-                        mprhs[1],
-                        mprhs[2],
-                        mprhs[3],
-                        mprhs[4],
-                        mprhs[5],
-                        mprhs[6],
-                        mprhs[7],
-                        mprhs[8]);
-
-  mlfRestorePreviousContext(0,
-                            8,
-                            mprhs[0],
-                            mprhs[1],
-                            mprhs[2],
-                            mprhs[3],
-                            mprhs[4],
-			    mprhs[5],
-                            mprhs[6],
-                            mprhs[7]);
-
-  plhs[0] = mplhs[0];
-  for (i = 1; i < 4 && i < nlhs; ++i)
-    {
-      plhs[i] = mplhs[i];
-    }
-  for (; i < 4; ++i)
-    {
-      mxDestroyArray(mplhs[i]);
-    }
-  mxDestroyArray(mprhs[8]);
-}
-
 
 /*
  * The function "MdoSimplex" is the implementation version of the "doSimplex"
@@ -244,7 +109,6 @@ void mlxDoSimplex(int nlhs, mxArray * plhs[], int nrhs, mxArray * prhs[])
 static mxArray * MdoSimplex(mxArray * * fval,
                             mxArray * * exitflag_mx,
                             mxArray * * output,
-                            int nargout_,
                             mxArray * funfcn_mx,
                             mxArray * x_in,
                             mxArray * printtype_mx,
@@ -253,7 +117,8 @@ static mxArray * MdoSimplex(mxArray * * fval,
                             mxArray * maxfun_mx,
                             mxArray * maxiter_mx,
                             mxArray * debugFlags_mx,
-                            mxArray * varargin)
+                            int nvararg,
+                            mxArray** pvararg)
 {
 DOTRACE("MdoSimplex");
 
@@ -284,14 +149,7 @@ DOTRACE("MdoSimplex");
       // numModelParams = prod(size(x));
       const int numModelParams = mxGetM(x_in) * mxGetN(x_in);
 
-      // Convert to inline function as needed.
-
-      // FIXME Since this requires "object-oriented" programming, we can't
-      // keep this and still use the MATLAB compiler
-
-      // %funfcn = fcnchk(funfcn,length(varargin));
-
-      MatlabFunction objective(funfcn_mx, varargin);
+      MatlabFunction objective(funfcn_mx, nvararg, pvararg);
 
       SimplexOptimizer opt(objective,
                            Mtx(x_in),
@@ -328,6 +186,76 @@ DOTRACE("MdoSimplex");
     }
 
   return (mxArray*) 0; // can't happen, but placate compiler
+}
+
+/*
+ * The function "mlxDoSimplex" contains the feval interface for the "doSimplex"
+ * M-function from file
+ * "/cit/rjpeters/science/psyphy/classmodels/matlab/doSimplex.m" (lines 1-237).
+ * The feval function calls the implementation version of doSimplex through
+ * this function. This function processes any input arguments and passes them
+ * to the implementation version of the function, appearing above.
+ */
+void mlxDoSimplex(int nlhs, mxArray * plhs[], int nrhs, mxArray * prhs[])
+{
+  const int NDECLARED = 8;
+
+  mxArray* mprhs[NDECLARED];
+  mxArray* mplhs[4];
+
+  if (nlhs > 4)
+    {
+      mexErrMsgTxt("Error: doSimplex was called with more "
+                   "than the declared number of outputs (4).");
+    }
+  if (nrhs < NDECLARED)
+    {
+      mexErrMsgTxt("Error: doSimplex was called with fewer "
+                   "than the decalred number of inputs.");
+    }
+
+  for (int i = 0; i < 4; ++i)
+    {
+      mplhs[i] = 0;
+    }
+  for (int i = 0; i < NDECLARED; ++i)
+    {
+      mprhs[i] = prhs[i];
+    }
+
+  mlfEnterNewContext(0, 8,
+                     mprhs[0], mprhs[1], mprhs[2], mprhs[3],
+                     mprhs[4], mprhs[5], mprhs[6], mprhs[7]);
+
+  int nvararg = nrhs - NDECLARED;
+  mxArray** pvararg = prhs + NDECLARED;
+
+  mplhs[0] = MdoSimplex(&mplhs[1],
+                        &mplhs[2],
+                        &mplhs[3],
+                        mprhs[0],
+                        mprhs[1],
+                        mprhs[2],
+                        mprhs[3],
+                        mprhs[4],
+                        mprhs[5],
+                        mprhs[6],
+                        mprhs[7],
+                        nvararg,
+                        pvararg);
+
+  mlfRestorePreviousContext(0, 8,
+                            mprhs[0], mprhs[1], mprhs[2], mprhs[3],
+                            mprhs[4], mprhs[5], mprhs[6], mprhs[7]);
+
+  plhs[0] = mplhs[0];
+  for (int i = 1; i < 4 && i < nlhs; ++i)
+    {
+      if (i < nlhs)
+        plhs[i] = mplhs[i];
+      else
+        mxDestroyArray(mplhs[i]);
+    }
 }
 
 static const char vcid_doSimplex_cc[] = "$Header$";
