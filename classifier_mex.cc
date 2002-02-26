ea5be57617b07687ef1b696f972d5e15ea75d0e6
Robert J. Peters <rjpcal@gmail.com>
2002-02-26 17:15:11 +0000

    Moved memory-management (i.e., mlfEnterNewContext() and mlfRestorePreviousContext()) into MexFcn::invoke().

--- classifier_mex.cc
+++ classifier_mex.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 2001-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Mar  8 09:49:21 2001
-// written: Mon Feb 25 16:43:11 2002
+// written: Tue Feb 26 09:14:27 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -213,14 +213,10 @@ namespace
 extern "C"
 void mlxClassifier(int nlhs, mxArray * plhs[], int nrhs, mxArray * prhs[])
 {
-  mlfEnterNewContext(0, NARGIN, prhs[0], prhs[1], prhs[2], prhs[3]);
-
   plhs[0] = Mclassifier(Mtx(prhs[0], Mtx::COPY), // model params
                         Mx::getString(prhs[1]), // model name
                         Mx::getString(prhs[2]), // action request
                         prhs[3]); // extra args struct
-
-  mlfRestorePreviousContext(0, NARGIN, prhs[0], prhs[1], prhs[2], prhs[3]);
 }
 
 
