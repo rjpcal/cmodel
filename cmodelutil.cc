1151c148d5e555a9f7d1a87d16de341be66224b6
Robert J. Peters <rjpcal@gmail.com>
2002-07-31 23:31:03 +0000

    Utility functions, currently just for extracting a set of stored exemplars from a raw array of model parameters (used by CModelSPC and CModelRxm).

---
+++ cmodelutil.cc
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////
+//
+// cmodelutil.cc
+//
+// Copyright (c) 2002-2002 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Jul 31 14:52:44 2002
+// written: Wed Jul 31 16:28:57 2002
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef CMODELUTIL_CC_DEFINED
+#define CMODELUTIL_CC_DEFINED
+
+#include "cmodel/cmodelutil.h"
+
+#include "cmodel/classifier.h"
+
+#include "mtx/mtx.h"
+
+#include "util/trace.h"
+
+void CModelUtil::clampRows(Mtx& src, int firstrow, int nrows, const Mtx& hilo)
+{
+DOTRACE("CModelUtil::clampRows");
+
+  for (int c = 0; c < src.ncols(); ++c)
+    {
+      double clo = hilo.at(0, c);
+      double chi = hilo.at(1, c);
+      for (int r = 0; r < nrows; ++r)
+        {
+          double v = src.at(firstrow+r,c);
+          if (v < clo) src.at(firstrow+r,c) = clo;
+          if (v > chi) src.at(firstrow+r,c) = chi;
+        }
+    }
+}
+
+Mtx CModelUtil::getStoredExemplars(const Slice& otherParams, int nstored,
+                                   const Mtx& hilo0, const Mtx& hilo1)
+{
+DOTRACE("CModelUtil::getStoredExemplars");
+
+  // reshape params into a stored exemplar matrix
+
+  Mtx storedExemplars =
+    Mtx(otherParams).as_shape(2*nstored, Classifier::DIM_OBJ_PARAMS);
+
+  clampRows(storedExemplars, 0, nstored, hilo0);
+
+  clampRows(storedExemplars, nstored, nstored, hilo1);
+
+  return storedExemplars;
+}
+
+Mtx CModelUtil::getHiLo(const Mtx& src)
+{
+DOTRACE("CModelUtil::getHiLo");
+
+  Mtx result(2, src.ncols());
+
+  for (int i = 0; i < src.ncols(); ++i)
+    {
+      result.at(0, i) = src.column(i).min();
+      result.at(1, i) = src.column(i).max();
+    }
+
+  return result;
+}
+
+static const char vcid_cmodelutil_cc[] = "$Header$";
+#endif // !CMODELUTIL_CC_DEFINED
---
+++ cmodelutil.h
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////
+//
+// cmodelutil.h
+//
+// Copyright (c) 2002-2002 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Jul 31 14:52:08 2002
+// written: Wed Jul 31 14:52:08 2002
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef CMODELUTIL_H_DEFINED
+#define CMODELUTIL_H_DEFINED
+
+class Mtx;
+class Slice;
+
+namespace CModelUtil
+{
+  void clampRows(Mtx& src, int firstrow, int nrows, const Mtx& hilo);
+
+  Mtx getStoredExemplars(const Slice& otherParams, int nstored,
+                         const Mtx& hilo0, const Mtx& hilo1);
+
+  Mtx getHiLo(const Mtx& src);
+}
+
+static const char vcid_cmodelutil_h[] = "$Header$";
+#endif // !CMODELUTIL_H_DEFINED
